b40def9b8be227e156bd31fce10ff766745774f8
Fernanda Ferreira <114786782+nandaferreira@users.noreply.github.com>
2024-11-12 16:19:44 -0300

    commit exc 01

--- trabalho final/segunda_lista_anna&fernanda.R
+++ trabalho final/segunda_lista_anna&fernanda.R
@@ -2,9 +2,189 @@
 #Nome: Fernanda Ferreira de Melo            Matrícula: 12211BCC043
 
 #--------------- Exercício 1 ---------------
+#---------Exercício 1---------
+library(ggplot2) #para plotar gráfico
+library(rpart) #para fazer o modelo da árvore de decisão
+library(rpart.plot) #plotar a árvore
+library(randomForest)#fazer a floresta aleatória
+library(dplyr)
+library(class)
+
+diabetes <- read.table(file = "diabetes.txt", header = TRUE, sep = ";") #ler o conjunto
+#Como criar o conjunto de teste e treinamento?
+
+diabetes
+str(diabetes)
+diabetes <- diabetes[,-1] #Tirei uma coluna porque a primeira era o ID e ela não me retorna nenhuma informação relevante, é só pra identificar o paciente mesmo
+diabetes$Diabetic <- as.factor(diabetes$Diabetic) #agora aparece que diabetic tem 2 levels, foi só pra refatorar
+n <- round(0.8*nrow(diabetes)) 
+set.seed(246)
+indices_treino <- sample(1:nrow(diabetes), size = n, replace = FALSE)
+
+#Treinamento: analiso o conjunto todo 
+#Teste: 20% restante
+
+treinamento <- diabetes[indices_treino,]
+teste <- diabetes[-indices_treino,]
+
+#----(a)---------
+summary(diabetes)
+str(diabetes)
+str(treinamento)
+str(diabetes)
+diabetes$Diabetic #vetor lógico (0 = não tem, 1 tem a doença)
+
+ggplot(data = treinamento, mapping = aes(x = Pregnancies, fill = factor(Diabetic))) +
+  geom_histogram(color = "black", bins = 30) +
+  theme_minimal() +
+  labs(x = "gravidezes", y = "contagem",fill = "diabetes")
+
+#a contagem de gravidezes parece ser um pouco maior em pacientes diabéticos, o que pode ser significar uma correlação entre a quantidade de gravidezes e o risco de desenvolver diabetes, provavelmente porque
+#durante a gestação o funcionamento metabólico da gestante é diferente.
+
+ggplot(data = treinamento, aes(x = PlasmaGlucose, fill = factor(Diabetic))) +
+  geom_histogram(color = "black", bins = 30) +
+  theme_minimal() +
+  labs(x = "glicose no sangue", y = "contagem", fill = "diabetes")
+#esse gráfico, como o esperado, indica que os números altos para diabetes estão diretamente relacionados com os níveis altos de açúcar no sangue do paciente. 
+
+ggplot(data = treinamento, aes(x = TricepsThickness, fill = factor(Diabetic)))+
+  geom_histogram(color = "black", bins = 30)+
+  theme_minimal()+
+  labs(x = "espessura do tríceps", y = "contagem", fill = "diabetes")
+# essa análise pode indicar que a espessura da pele do tríceps, que é também um indicativo de gordura corporal, 
+#pode não ser um fator diretamente associado à presença da doença nesse banco de dados.
+
+ggplot(data = treinamento, aes(x = SerumInsulin, fill = factor(Diabetic)))+
+  geom_histogram(color = "black", bins = 30)+
+  theme_minimal()+
+  labs(x = "concentração de insulina", y = "contagem", fill = "diabetes")
+
+#o plot da insulina mostra que tem uma variação significativa nos dois grupos, mas pode ser que níveis mais altos 
+#de insulina estejam mais relacionados com resistência à insulina, que é uma condição comumente associada aos diabéticos de tipo 2
+
+ggplot(data = treinamento, aes(x = BMI, fill = factor(Diabetic)))+
+  geom_histogram(color = "black", bins = 30)+
+  theme_minimal()+
+  labs(x = "índice de massa corporal", y = "contagem", fill = "diabetes")
+
+#aqui faz sentido o maior número de casos ser em pessoas com um IMC mais elevado pois ele é um fator de risco conhecido para 
+#diabetes, por causa do excesso de peso que pode afetar a sensibilidade à insulina.
 
 
+ggplot(data = treinamento, aes(x = DiabetesPedigree, fill = factor(Diabetic)))+
+  geom_histogram(color = "black", bins = 30)+
+  theme_minimal()+
+  labs(x = "tendência ao desenvolvimento de diabetes", y = "contagem", fill = "diabetes")
+#esse gráfico indica que pacientes diabéticos tem um nível mais aparente de históricos familiares que apresentam casos da doença.
 
+ggplot(data = treinamento, aes(x = Age, fill = factor(Diabetic)))+
+  geom_histogram(color = "black", bins = 30)+
+  theme_minimal()+
+  labs(x = "idade", y = "contagem", fill = "Diabetes")
+#já que  a capacidade de resposta à insulina pode diminuir ao longo dos anos, a idade mostra que pacientes diabéticos
+#tendem a ser mais velhos.
+
+#Concluindo: os gráficos acima sugerem que fatores como níveis elevados de glicose no sangue, IMC mais alto, idade avançada 
+#e histórico familiar de diabetes têm uma correlação mais clara com a presença de diabetes. Porém, variáveis 
+#como espessura do tríceps parecem ter uma correlação menos evidente.
+
+#----(b)---------
+#Usar só o conjunto treinamento !!
+
+#Modelo de árvore de decisão
+
+modelo.arvore <- rpart(formula = Diabetic~ ., data = treinamento, method = "class") #a formula é o diagnostico em função de tudo (Diabetic ~.,)
+print(modelo.arvore)
+rpart.plot(modelo.arvore, extra = 101) #extra = 101 exibe a classe predita em cada nó da árvore, juntamente com o número de observações de treinamento que caem em cada nó.
+previsao <- predict(modelo.arvore, newdata = teste, type = "class")
+print(previsao)
+#Função diagnóstico
+
+diagnostico <- function(Pregnancies, PlasmaGlucose, DiastolicBloodPressure, TricepsThickness, SerumInsulin, BMI, DiabetesPedigree, Age){
+  if(Pregnancies < 2){
+    return("Negativo")
+  }else{
+    if(BMI < 22){
+      return("Negativo")
+    } else{
+      if(SerumInsulin < 52){
+        if(Age < 36){
+          return("Negativo")
+        }else{
+          return("Positivo")
+        }
+      }else{
+        if(Age <36){
+          if(BMI >= 36 && PlasmaGlucose < 96){
+            return("Negativo")
+          } else if(BMI >= 36 && PlasmaGlucose > 96){
+            return("Positivo")
+          }
+        }else{
+          return("Negativo")
+        }
+      }
+    }
+  }
+}
+
+
+teste$Diagnostico_Previsto <- mapply(diagnostico,
+                                     teste$Pregnancies,
+                                     teste$PlasmaGlucose,
+                                     teste$DiastolicBloodPressure,
+                                     teste$TricepsThickness,
+                                     teste$SerumInsulin,
+                                     teste$BMI,
+                                     teste$DiabetesPedigree,
+                                     teste$Age)
+
+mostrar_diagnostico <-(teste[, c("PlasmaGlucose", "BMI", "DiastolicBloodPressure", "Age", "Diagnostico_Previsto")])
+print(mostrar_diagnostico)
+
+#Acurácia do modelo
+teste$Diabetic <- factor(teste$Diabetic, levels = c(0, 1), labels = c("Negativo", "Positivo"))
+acuracia <- mean(teste$Diagnostico_Previsto == teste$Diabetic)
+round(acuracia*100,2)
+
+
+#----(c)-----
+# Floresta aleatória
+set.seed(806) 
+indices_treino <- sample(1:nrow(diabetes), size = n, replace = FALSE)
+treino <- diabetes[indices_treino,]
+teste <- diabetes[-indices_treino,]
+
+treino$Diabetic <- as.factor(treino$Diabetic)
+teste$Diabetic <- as.factor(teste$Diabetic)
+floresta <- randomForest(formula = Diabetic ~ ., data = treino, ntree = 200)
+print(floresta)
+
+previsao.floresta <- predict(floresta, newdata = teste, type = "class")
+print(previsao.floresta)
+acuracia_floresta <- mean(previsao.floresta == teste$Diabetic)
+round(acuracia_floresta * 100, 2)
+
+#----(d)-----
+#Árvore
+table(previsao, teste$Diabetic)
+probabilidade_arvore <- mean(previsao == teste$Diabetic)
+probabilidade_arvore
+
+#Floresta
+table(previsao.floresta, teste$Diabetic) 
+probabilidade_floresta <- mean(previsao.floresta == teste$Diabetic)
+probabilidade_floresta
+
+#----(e)-----
+#Com base nos resultados obtidos percebe-se que o modelo de floresta aleatória se saiu relativamente 
+#melhor que o modelo de árvore de decisão. Isso porque na floresta obtivemos uma taxa de acerto 
+#de "verdadeiros positivos" maior que na árvore, ou seja, ela foi mais capaz de identificar 
+#os resultados corretos, além de que ela também uma obteve uma acurácia maior. Sendo assim,
+#a Floresta Aleatória foi o mais eficaz na identificação correta de pacientes com diabetes e é 
+#mais viavél de ser usada neste conjunto de dados. (porém se for apenas para interpretar as informações,
+#a árvore de decisão é mais intuitiva)
 
 #--------------- Exercício 2 ---------------
 dados <- read.csv(file = "cerebelo.csv", header = TRUE, sep = ",")
